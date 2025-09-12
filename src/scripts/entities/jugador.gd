5dfb110618f6039bd6aa78ac81dbae96a7b919c3
Carlos Mauricio Fabris <107259585+maurifabris@users.noreply.github.com>
2025-09-11 23:25:52 -0300

    JUGADOR DE PRUEBAS

---
+++ src/scripts/entities/jugador.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animated_sprite_2d.play("jump")
+
+	# Handle jump.
+	if Input.is_action_just_pressed("JUMP") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("IZQUIERDA", "DERECHA")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
+#	animaciones, falta hacer que se cambie de direccion INCLUSO SI SE TOCA LA TECLA EN EL AIRE
+	if Input.is_action_pressed("DERECHA") and is_on_floor():
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("run")
+	elif Input.is_action_pressed("IZQUIERDA") and is_on_floor():
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play("run")
+	elif not (Input.is_action_pressed("IZQUIERDA") 
+		   or Input.is_action_pressed("DERECHA") 
+		   or Input.is_action_pressed("JUMP")) and is_on_floor():
+		animated_sprite_2d.play("idle")
+	
+		
+	
+
+
+
+	move_and_slide()
