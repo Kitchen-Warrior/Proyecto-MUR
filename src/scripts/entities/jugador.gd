832384e1ee80375841a027c6021f47e6c42727ea
Carlos Mauricio Fabris <107259585+maurifabris@users.noreply.github.com>
2025-09-14 20:50:49 -0300

    pruebas de colisiones

--- src/scripts/entities/jugador.gd
+++ src/scripts/entities/jugador.gd
@@ -1,33 +1,28 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
+var health: int = 3
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite_2d.play("jump")
 
-	# Handle jump.
+	# Salto
 	if Input.is_action_just_pressed("JUMP") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimiento horizontal
 	var direction := Input.get_axis("IZQUIERDA", "DERECHA")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	
-#	animaciones, falta hacer que se cambie de direccion INCLUSO SI SE TOCA LA TECLA EN EL AIRE
+	# Animaciones
 	if Input.is_action_pressed("DERECHA") and is_on_floor():
 		animated_sprite_2d.flip_h = false
 		animated_sprite_2d.play("run")
@@ -39,9 +34,25 @@ func _physics_process(delta: float) -> void:
 		   or Input.is_action_pressed("JUMP")) and is_on_floor():
 		animated_sprite_2d.play("idle")
 	
-		
-	
-
-
-
+	# Movimiento + colisiones
 	move_and_slide()
+	_check_spike_damage()
+
+
+################### DAÑO ###################
+
+func _check_spike_damage():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().name == "spikes":
+			take_damage()
+
+func take_damage() -> void:
+	health -= 1
+	print("Vida restante: ", health)
+	if health <= 0:
+		die()
+
+func die() -> void:
+	print("¡Game Over!")
+	queue_free()
