093117190349540738ae141eb6ddbdb249ec2058
Carlos Mauricio Fabris <107259585+maurifabris@users.noreply.github.com>
2025-09-17 15:22:33 -0300

    daño por ataque de enemigo
    no consigo que solo dañe en frames de golpe

--- src/scenes/entities/enemies.gd
+++ src/scenes/entities/enemies.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var ataques: Area2D = $ataques
+
+@onready var sprites_enemies: AnimatedSprite2D = $CharacterBody2D/sprites_enemies
+
+var damage_frames: Array = [3, 5]
+
+
+
+
+
 func _ready() -> void:
 	pass
 	
@@ -9,6 +19,25 @@ func _process(delta: float) -> void:
 	
 
 
+# Si el jugador entra en el area del esqueleto pierde vida, falta hacer que el 
+# esqueleto solo dañe si toca el fame de ataque 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	print("funciona hitbox body")
-	body.take_damage()
+	pass
+	#print("funciona hitbox body")
+	#if body.name == "jugador_mau":
+		#sprites_enemies.play("atack")
+	#if ataques.monitoring and body.name == "jugador_mau":
+		#body.take_damage()
+		
+
+
+func _on_ataques_body_entered(body: Node2D) -> void:
+	print("golpe de espada")
+	if sprites_enemies.animation == "atack":
+		if sprites_enemies.frame in damage_frames:
+			ataques.monitoring = true  # activa colisiones
+		else:
+			ataques.monitoring = false # desactiva colisiones.
+	if ataques.monitoring and body.name == "jugador_mau":
+		body.take_damage()
+			
