4ca4e67355b692819ae0bc31ea35d3166c8c6f9f
Carlos Mauricio Fabris <107259585+maurifabris@users.noreply.github.com>
2025-09-18 20:34:39 -0300

    doble salto

--- src/scripts/entities/jugador.gd
+++ src/scripts/entities/jugador.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var health: int = 5
-
+var jump_count: int = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -15,9 +15,12 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("jump")
 
 	# Salto
-	if Input.is_action_just_pressed("JUMP") and is_on_floor():
+	if Input.is_action_just_pressed("JUMP") and  (is_on_floor() or jump_count == 0):
 		velocity.y = JUMP_VELOCITY
-		
+		jump_count += 1
+	
+	if is_on_floor():
+		jump_count = 0
 	# Movimiento horizontal
 	var direction := Input.get_axis("IZQUIERDA", "DERECHA")
 	if direction:
