252c2f85eb65fa4426b1775d0abe95186f7f4609
Alejandro Ovando <ale_0vando05@outlook.com>
2025-09-18 20:37:01 -0300

    tamaño

--- src/scripts/entities/jugador.gd
+++ src/scripts/entities/jugador.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 120.0
+const SPEED = 135.0
 const JUMP_VELOCITY = -250.0
 var health: int = 5
 
@@ -13,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite_2d.play("jump")
+		
 
 	# Salto
 	if Input.is_action_just_pressed("JUMP") and is_on_floor():
