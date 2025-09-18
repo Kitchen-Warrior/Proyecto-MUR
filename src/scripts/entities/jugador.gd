99afe45d0920fadfe1ed178b7a6ff64c585016b8
Alejandro Ovando <ale_0vando05@outlook.com>
2025-09-18 20:03:56 -0300

    modificacion de escala jugador mau

--- src/scripts/entities/jugador.gd
+++ src/scripts/entities/jugador.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 120.0
+const JUMP_VELOCITY = -250.0
 var health: int = 5
 
 
